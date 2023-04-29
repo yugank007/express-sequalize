e29b132013223a7d37d369a0d7cf97fac0289279
yugank007 <yugank007@gmail.com>
2023-04-29 14:22:35 +0530

    Init commit

---
+++ src/Modules/events/entities/event.entity.ts
@@ -0,0 +1,29 @@
+import {
+  Table,
+  Column,
+  Model,
+  AutoIncrement,
+  PrimaryKey,
+  HasMany
+} from 'sequelize-typescript';
+import { ModelAttributeColumnOptions } from 'sequelize';
+import Workshop from './workshop.entity';
+
+@Table({
+  updatedAt: false,
+})
+export default class Event extends Model {
+  @AutoIncrement
+  @PrimaryKey
+  @Column
+  declare id: number;
+
+  @Column
+  name: string;
+
+  @Column({ type: 'datetime' } as ModelAttributeColumnOptions)
+  declare createdAt: Date;
+  
+  @HasMany(() => Workshop)
+  workshops: Workshop[];
+}
